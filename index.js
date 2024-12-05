9f08cb0d866db7d8a431fa8c06dd5dbf731ad65d
Mohsinkhanafridi <96548013+Mohsinkhanafridi@users.noreply.github.com>
2024-12-05 11:10:48 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import CommentRoute from "./Routes/CommentsRoute.js";
+import cors from "cors"
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
+app.use(cors());
 
 app.get("/",(req, res)=>{
     res.status(200).send("mohsin khan afridi");
